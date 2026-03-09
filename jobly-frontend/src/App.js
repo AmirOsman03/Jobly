e9fafe2cbcbb6d4edf3139b318623249e310391d
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2026-03-09 23:56:57 +0100

    Added light and dark theme option

--- jobly-frontend/src/App.js
+++ jobly-frontend/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import Home from "./pages/Home";
-import {CssBaseline} from "@mui/material";
+import {Button, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import BrightnessHighIcon from '@mui/icons-material/BrightnessHigh';
+import Brightness3Icon from '@mui/icons-material/Brightness3';
 
 const App = () => {
+    const [mode, setMode] = useState("light");
+
+    const theme = useMemo(() =>
+        createTheme({
+            palette: {
+                mode: mode,
+            },
+        }), [mode]
+    );
+
+    const toggleTheme = () => {
+        setMode(prev => (prev === "light" ? "dark" : "light"));
+    }
+
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <Home/>
-        </>
+            <div style={{padding: 20}}>
+                <Button
+                variant="contained"
+                startIcon={mode === "light" ? <Brightness3Icon/> : <BrightnessHighIcon/>}
+                onClick={toggleTheme}
+                >
+                    {mode === "light" ? "Dark Mode" : "Light Mode"}
+                </Button>
+                <Home/>
+            </div>
+        </ThemeProvider>
     );
 };
 
